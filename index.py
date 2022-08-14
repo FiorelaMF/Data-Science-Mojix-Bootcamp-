4c56071eaef7aa2b885dd39388f2d90ac92f0bad
FiorelaMF <fiorela.mafer140@gmail.com>
2022-08-14 12:36:51 -0500

    Add code sections

--- index.py
+++ index.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.title('10 Cool Beginner Python Tricks That Will Make Your Life Easier')
+st.write('Simple but effective tips for every python lovers')
+
+st.write("The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can surprise you with their amazing capabilities. \nIn today’s article, I will discuss 10 Python tips and tricks that will be really helpful for beginners to write more compact code. Knowing these tips and tricks will definitely save you some valuable time.")
+
+st.subheader('1. Walrus operator')
+st.write("The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.")
+st.code('Mylist = [1,2,3]\nif(l := len(mylist) > 2) \n \tprint(l)')
+st.write('Output:')
+st.code('3')
