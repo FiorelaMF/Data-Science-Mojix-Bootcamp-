0c1624a46aea22d93552f989673f70a694dc4783
FiorelaMF <fiorela.mafer140@gmail.com>
2022-08-21 00:00:34 -0500

    Discrepancy Data Analysis

---
+++ csv_reader.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 16 18:10:58 2022
+@author: Fiorela
+"""
+
+import streamlit as st 
+'''
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.title('CSV Reader')
+file = st.file_uploader('Upload a csv', type='csv')
+
+if file:
+    df = pd.read_csv(file)
+    st.dataframe(df)
+    st.markdown('---')
+    
+    fig1 = plt.figure(figsize=(10,4))
+    sns.countplot(x='Pclass', data=df)
+    
+    st.pyplot(fig1)
+    
+    fig2 = plt.figure(figsize=(10,4))
+    sns.countplot(x='Pclass', y='Age', data=df)
+    
+    st.pyplot(fig2)
+    '''
